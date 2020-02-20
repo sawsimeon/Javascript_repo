fbc33e29fdc63690b9fa32936bd43d17644b2b05
Saw Simeon <doe_doe207@hotmail.com>
2020-02-20 16:46:31 +0700

    Building the array with push

---
+++ building_array_with_push.js
@@ -0,0 +1,9 @@
+var landmarks = [];
+landmarks.push("My house");
+landmarks.push("Front path");
+landmarks.push("Flickering streetlamp");
+landmarks.push("Leaky fire hydrant");
+landmarks.push("Fire station");
+landmarks.push("Car rescue center");
+landmarks.push("My old school");
+landmarks.push("My friend's house");
